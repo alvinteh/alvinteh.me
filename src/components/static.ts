3aa3d9b66d4ada125f5014817384902645903971
Alvin Teh <i@alvinteh.me>
2023-12-07 00:32:14 +0800

    Implemented About section

--- src/components/static.ts
+++ src/components/static.ts
@@ -1,6 +1,25 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 const cubicBezier = 'cubic-bezier(0.525, 0.06, 0.11, 0.995)';
+const pageTransitionDuration = 400;
+
+const fadeInAnimation = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
+const wipeInAnimation = keyframes`
+  0% {
+    max-width: 0;
+  }
+  100% {
+    max-width: 100%;
+  }
+`;
 
 const FullPageWrapper = styled.div`
   position: absolute;
@@ -13,10 +32,30 @@ const FullPageWrapper = styled.div`
 
 const PageWrapper = styled.div`
   position: relative;
+  box-sizing: border-box;
+  padding: 50px 40px 0;
+  min-height: 100vh;
+  animation: ${fadeInAnimation} 1ms ${cubicBezier} ${pageTransitionDuration}ms 1 backwards;
+`;
+
+const PageTitle = styled.h1`
+  margin: 0 0 10px;
+  color: #ffffff;
+  font-size: 5rem;
+  font-family: 'Barlow Condensed', sans-serif;
+  font-weight: 600;
+  opacity: 0.85;
+  overflow: hidden;
+  text-transform: uppercase;
+  animation: ${wipeInAnimation} 1600ms ${cubicBezier} ${pageTransitionDuration}ms 1 backwards;
 `;
 
 export {
   cubicBezier,
+  fadeInAnimation,
+  pageTransitionDuration,
+
   FullPageWrapper,
-  PageWrapper
+  PageWrapper,
+  PageTitle
 };
