51b9bc830c14547497da592a68daca27b149eea6
Alvin Teh <i@alvinteh.me>
2024-01-11 23:02:13 +0800

    Implemented gallery updates on drag rather than drag end

--- src/routes/Visual/scenes/GalleryScene/components/Gallery.tsx
+++ src/routes/Visual/scenes/GalleryScene/components/Gallery.tsx
@@ -2,7 +2,7 @@ import gsap from 'gsap';
 import { Draggable } from 'gsap/Draggable';
 import { InertiaPlugin } from 'gsap/InertiaPlugin';
 import { useGSAP } from '@gsap/react';
-import React, { useCallback, useContext, useEffect, useMemo, useRef, useState } from 'react';
+import React, { createRef, useCallback, useContext, useEffect, useMemo, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { v4 as uuid } from 'uuid';
 
@@ -13,6 +13,12 @@ import { Image } from '../types';
 interface GalleryImage {
   id : string;
   image: Image;
+  x: number;
+  y: number;
+}
+
+interface GalleryLayoutElement {
+  element: HTMLDivElement;
   x: number;
   y: number;
 }
@@ -91,19 +97,43 @@ const Caption = styled.div`
 
 const galleryItemPadding = 5;
 
-const GalleryImage = ({ id, image, height, padding, x, y, isClickable, onClick }: {
+const getElementTranslation = (element: HTMLElement): [number, number, number] => {
+  const translates: number[] = element.style.transform.split(/\w+\(|\);?/)[1].split(/,\s?/g)
+    .map((s: string) => { return Number.parseFloat(s.replace('px', '')); });
+
+  return [translates[0], translates[1], translates[2]];
+};
+
+const setElementTranslation = (element: HTMLElement, { x, y, z }: { x?: number, y?: number, z?: number }): void => {
+  const translates: number[] = getElementTranslation(element);
+
+  if (x) {
+    translates[0] = x;
+  }
+  if (y) {
+    translates[1] = y;
+  }
+  if (z) {
+    translates[2] = z;
+  }
+
+  element.style.transform = `translate3d(${translates[0]}px, ${translates[1]}px, ${translates[2]}px)`
+};
+
+const GalleryImage = ({ id, innerRef, image, height, padding, initialX, initialY, isClickable, onClick }: {  
   id: string,
+  innerRef: React.RefObject<HTMLDivElement>,
   image: Image,
   height: number,
   padding: number,
-  x: number,
-  y: number,
+  initialX: number,
+  initialY: number,
   isClickable: boolean,
   onClick: (id: string) => void,
 }) => {
   const style: React.CSSProperties = {
-    transform: `translate3d(${x}px, ${y}px, 0)`,
     pointerEvents: isClickable ? 'auto' : 'none',
+    transform: `translate3d(${initialX}px, ${initialY}px, 0)`,
   };
   const handleClick = (): void => {
     onClick(id);
@@ -112,6 +142,7 @@ const GalleryImage = ({ id, image, height, padding, x, y, isClickable, onClick }
   return (
     <ImageWrapper
       data-id={id}
+      ref={innerRef}
       $aspectRatio={image.aspectRatio}
       $padding={padding}
       $height={height}
@@ -178,8 +209,7 @@ const FullImageViewer = ({ isActive, galleryImage, galleryItemHeight, galleryRef
     const image: Image = galleryImage.image;
     const galleryElement: HTMLDivElement = galleryRef.current;
     const galleryBounds: DOMRect = galleryElement.getBoundingClientRect();
-    const galleryTranslate: number[] = galleryElement.style.transform.split(/\w+\(|\);?/)[1]
-      .split(/,\s?/g).map((s: string) => { return Number.parseFloat(s.replace('px', '')); });
+    const galleryTranslate: number[] = getElementTranslation(galleryElement);
     const galleryTranslateX: number = galleryTranslate[0];
     const galleryTranslateY: number = galleryTranslate[1];
 
@@ -277,9 +307,9 @@ const FullImageViewer = ({ isActive, galleryImage, galleryItemHeight, galleryRef
 
 const Gallery = ({ images, itemHeight, scrollTop }: { images: Image[], itemHeight: number, scrollTop: number }) => {
   const galleryRef =  useRef<HTMLDivElement>() as React.MutableRefObject<HTMLDivElement>;
+  const galleryImageRefs = useRef<React.RefObject<HTMLDivElement>[]>([]);
 
   const [galleryImages, setGalleryImages] = useState<Record<string, GalleryImage>>({});
-  const [galleryLayout, setGalleryLayout] = useState<GalleryImage[][]>([]);
   const [activeGalleryImage, setActiveGalleryImage] = useState<GalleryImage>();
   const [isDragging, setIsDragging] = useState<boolean>(false);
   const { isOverlayToggled } = useContext(LayoutContext);
@@ -299,8 +329,6 @@ const Gallery = ({ images, itemHeight, scrollTop }: { images: Image[], itemHeigh
   }, [itemHeight]);
 
   const handleDrag = useCallback(function(): void {
-    setIsDragging(false);
-
     // @ts-expect-error We can ignore the linting issue as this is set by GSAP
     const draggable: Draggable = this as Draggable;
     // Note: we do not use .deltaX/Y as those are inaccurate
@@ -312,87 +340,116 @@ const Gallery = ({ images, itemHeight, scrollTop }: { images: Image[], itemHeigh
     const galleryLeft: number = draggable.x * -1;
     const galleryRight: number = galleryLeft + galleryRef.current.clientWidth;
 
-    const newGalleryImages: Record<string, GalleryImage> = Object.assign({}, galleryImages);
-    const updatedGalleryImages: GalleryImage[] = [];
-    let newGalleryLayout: GalleryImage[][] = galleryLayout.slice();
+    const unsortedGalleryLayout: Record<number, GalleryLayoutElement[]> = {};
+    let galleryLayout: GalleryLayoutElement[][] = [];
+    const galleryImageYs: number[] = [];
+
+    // Build the gallery layout by checking the translate values of each element
+    for (const galleryImageRef of galleryImageRefs.current) {
+      // We can ignore the linting issue as we have set up this ref
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const galleryImageElement: HTMLDivElement = galleryImageRef.current!;
+      const [x, y] = getElementTranslation(galleryImageElement);
+
+      if (!galleryImageYs.includes(y)) {
+        galleryImageYs.push(y);
+        unsortedGalleryLayout[y] = [];
+      }
+
+      unsortedGalleryLayout[y].push({
+        element: galleryImageElement,
+        x,
+        y,
+      });
+    }
+
+    galleryImageYs.sort((a: number, b: number) => { return a - b});
+    
+    for (const galleryImageY of galleryImageYs) {
+      unsortedGalleryLayout[galleryImageY].sort((a, b) => a.x - b.x); 
+
+      galleryLayout.push(unsortedGalleryLayout[galleryImageY]);
+    }
+
+    // Calculate changes to be made
+    const updatedGalleryLayoutElements: GalleryLayoutElement[] = [];
 
     if (deltaY > 0) {
       // Check if first item in each row is within Y bounds, starting with top row
-      while (newGalleryLayout[0][0].y + galleryImageHeight > galleryTop) {
+      while (galleryLayout[0][0].y + galleryImageHeight > galleryTop) {
         // Move bottommost items to top edge (using top item's Y)
-        const newY: number = newGalleryLayout[0][0].y - galleryImageHeight;
+        const newY: number = galleryLayout[0][0].y - galleryImageHeight;
 
-        newGalleryLayout[newGalleryLayout.length - 1].forEach((galleryImage: GalleryImage): void => {
-          galleryImage.y = newY;
-          updatedGalleryImages.push(galleryImage);
+        galleryLayout[galleryLayout.length - 1].forEach((galleryLayoutElement: GalleryLayoutElement): void => {
+          galleryLayoutElement.y = newY;
+          updatedGalleryLayoutElements.push(galleryLayoutElement);
         });
 
         // Update gallery layout
-        newGalleryLayout = newGalleryLayout.slice(-1).concat(newGalleryLayout.slice(0, -1));
+        galleryLayout = galleryLayout.slice(-1).concat(galleryLayout.slice(0, -1));
       }
     }
     else if (deltaY < 0) {
       // Check if first item in each row is within Y bounds, starting with bottom row
-      while (newGalleryLayout[newGalleryLayout.length - 1][0].y < galleryBottom) {
+      while (galleryLayout[galleryLayout.length - 1][0].y < galleryBottom) {
         // Move topmost items to bottom edge (using bottom item's Y)
-        const newY: number = newGalleryLayout[newGalleryLayout.length - 1][0].y + galleryImageHeight;
+        const newY: number = galleryLayout[galleryLayout.length - 1][0].y + galleryImageHeight;
 
-        newGalleryLayout[0].forEach((galleryImage: GalleryImage): void => {
-          galleryImage.y = newY;
-          updatedGalleryImages.push(galleryImage);
+        galleryLayout[0].forEach((galleryLayoutElement: GalleryLayoutElement): void => {
+          galleryLayoutElement.y = newY;
+          updatedGalleryLayoutElements.push(galleryLayoutElement);
         });
 
         // Update gallery layout
-        newGalleryLayout = newGalleryLayout.slice(1).concat(newGalleryLayout.slice(0, 1));
+        galleryLayout = galleryLayout.slice(1).concat(galleryLayout.slice(0, 1));
       }
     }
     if (deltaX > 0) {
       // Check if each item in each row is within X bounds, starting with first/leftmost item
-      for (let i = 0; i < newGalleryLayout.length; i++) {
-        while (newGalleryLayout[i][0].x + getGalleryImageWidth(newGalleryLayout[i][0].image) > galleryLeft) {
+      for (let i = 0; i < galleryLayout.length; i++) {
+        while (galleryLayout[i][0].x + galleryLayout[i][0].element.clientWidth > galleryLeft) {
           // Move last/rightmost item to left edge (using leftmost item's X)
-          const cmpGalleryImage: GalleryImage = newGalleryLayout[i][0];
-        
-          const galleryImage: GalleryImage = newGalleryLayout[i][newGalleryLayout[i].length - 1];
-          galleryImage.x = cmpGalleryImage.x - getGalleryImageWidth(galleryImage.image);
-          updatedGalleryImages.push(galleryImage);
+          const cmpGalleryLayoutElement: GalleryLayoutElement = galleryLayout[i][0];
+          const galleryLayoutElement: GalleryLayoutElement = galleryLayout[i][galleryLayout[i].length - 1];
+          const newX: number = cmpGalleryLayoutElement.x - galleryLayoutElement.element.clientWidth;
+          galleryLayoutElement.x = newX;
+          updatedGalleryLayoutElements.push(galleryLayoutElement);
 
           // Update gallery layout
-          newGalleryLayout[i] = newGalleryLayout[i].slice(-1).concat(newGalleryLayout[i].slice(0, -1));
+          galleryLayout[i] = galleryLayout[i].slice(-1).concat(galleryLayout[i].slice(0, -1));
         }
       }
     }
     else if (deltaX < 0) {
       // Check if last/rightmost item in each row is within X bounds
-      for (let i = 0; i < newGalleryLayout.length; i++) {
-        while (newGalleryLayout[i][newGalleryLayout[i].length - 1].x < galleryRight) {
+      for (let i = 0; i < galleryLayout.length; i++) {
+        while (galleryLayout[i][galleryLayout[i].length - 1].x < galleryRight) {
           // Move first/leftmost item to right edge (using rightmost item's X)
-          const cmpGalleryImage: GalleryImage = newGalleryLayout[i][newGalleryLayout[i].length - 1];
-
-          const galleryImage: GalleryImage = newGalleryLayout[i][0];
-          galleryImage.x = cmpGalleryImage.x + getGalleryImageWidth(cmpGalleryImage.image);
-          updatedGalleryImages.push(galleryImage);
+          const cmpGalleryLayoutElement: GalleryLayoutElement = galleryLayout[i][galleryLayout[i].length - 1];
+          const galleryLayoutElement: GalleryLayoutElement = galleryLayout[i][0];
+          const newX: number = cmpGalleryLayoutElement.x + cmpGalleryLayoutElement.element.clientWidth;
+          galleryLayoutElement.x = newX;
+          updatedGalleryLayoutElements.push(galleryLayoutElement);
 
           // Update gallery layout
-          newGalleryLayout[i] = newGalleryLayout[i].slice(1).concat(newGalleryLayout[i].slice(0, 1));
+          galleryLayout[i] = galleryLayout[i].slice(1).concat(galleryLayout[i].slice(0, 1));
         }
       }
     }
 
-    // Update state
-    if (updatedGalleryImages.length > 0) {
-      if (!import.meta.env.PROD) {
-        console.info('Updated gallery layout', newGalleryLayout);
-      }
-
-      for (const galleryImage of updatedGalleryImages) {
-        newGalleryImages[galleryImage.id] = galleryImage;
-      }
-
-      setGalleryImages(newGalleryImages);
-      setGalleryLayout(newGalleryLayout);
+    // Perform DOM updates
+    for (const galleryLayoutElement of updatedGalleryLayoutElements) {
+      setElementTranslation(galleryLayoutElement.element, { x: galleryLayoutElement.x, y: galleryLayoutElement.y });
     }
-  }, [galleryImages, galleryLayout, galleryImageHeight, getGalleryImageWidth]);
+  }, [galleryImageHeight]);
+
+  const handleDragStart = (): void => {
+    setIsDragging(true);
+  };
+
+  const handleDragEnd = (): void => {
+    setIsDragging(false);
+  };
 
   const handleGalleryImageClick = useCallback((id: string): void => {
     if (isDragging) {
@@ -408,13 +465,11 @@ const Gallery = ({ images, itemHeight, scrollTop }: { images: Image[], itemHeigh
     const galleryHeight: number = galleryElement.clientHeight;
     
     const galleryImages: Record<string, GalleryImage> = {};
-    const galleryLayout: GalleryImage[][] = [];
     let imagesIndex = 0;
     
     // Populate gallery with images, buffering an additional image outside the gallery viewport
     for (let currentY = -galleryImageHeight; currentY < galleryHeight + galleryImageHeight;
       currentY += galleryImageHeight) {
-      const galleryImageRow: GalleryImage[] = [];
       
       for (let currentX = -galleryImageMaxWidth; currentX < galleryWidth + galleryImageMaxWidth;
         currentX += getGalleryImageWidth(images[imagesIndex]), imagesIndex++) {
@@ -432,14 +487,10 @@ const Gallery = ({ images, itemHeight, scrollTop }: { images: Image[], itemHeigh
         };
 
         galleryImages[galleryImage.id] = galleryImage;
-        galleryImageRow.push(galleryImage);
       }
-
-      galleryLayout.push(galleryImageRow);
     }
 
     setGalleryImages(galleryImages);
-    setGalleryLayout(galleryLayout);
   }, [images, itemHeight, galleryImageHeight, galleryImageMaxWidth, getGalleryImageWidth]);
 
   useEffect((): void => {
@@ -459,26 +510,31 @@ const Gallery = ({ images, itemHeight, scrollTop }: { images: Image[], itemHeigh
       type: 'x,y',
       inertia: true,
       zIndexBoost: false,
-      onDragStart: (): void => {
-        setIsDragging(true);
-      },
-      onDragEnd: handleDrag,
+      onDragStart: handleDragStart,
+      onDrag: handleDrag,
+      onDragEnd: handleDragEnd,
     });
   }, [galleryImages]);
 
-  const imageElements = Object.values(galleryImages).map((galleryImage: GalleryImage) => {
-    return <GalleryImage
+  const imageElements = [];
+  
+  for (let i = 0, galleryImageValues = Object.values(galleryImages); i < galleryImageValues.length; i++) {
+    const galleryImage = galleryImageValues[i];
+    galleryImageRefs.current[i] = createRef<HTMLDivElement>();
+
+    imageElements.push(<GalleryImage
       key={galleryImage.id}
       id={galleryImage.id}
+      innerRef={galleryImageRefs.current[i]}
       image={galleryImage.image}
       padding={galleryItemPadding}
       height={itemHeight}
-      x={galleryImage.x}
-      y={galleryImage.y}
+      initialX={galleryImage.x}
+      initialY={galleryImage.y}
       isClickable={!isDragging}
       onClick={handleGalleryImageClick}
-    />;
-  });
+    />);
+  }
 
   return (
     <>
