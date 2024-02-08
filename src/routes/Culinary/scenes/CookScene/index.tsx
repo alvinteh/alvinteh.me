6a6bb31af7cf2a3bb719653acd6b358a550d99d2
Alvin Teh <i@alvinteh.me>
2024-02-08 12:45:00 +0800

    Optimized animation by using x instead of left

--- src/routes/Culinary/scenes/CookScene/index.tsx
+++ src/routes/Culinary/scenes/CookScene/index.tsx
@@ -553,7 +553,7 @@ const CookScene = ({ sceneIndex }: SceneProps) => {
 
       timeline.from(extraDishElement, {
         y: '105vh', // Leave additional allowance given the rotation
-        left: 'calc(50% - 7vw - 10px)',
+        x: 'calc(50% - 7vw - 10px)',
         ease: 'power1.out',
         duration: animationDurations.FAST,
       });
@@ -566,7 +566,7 @@ const CookScene = ({ sceneIndex }: SceneProps) => {
 
       timeline.to(extraDishElement, {
         y: '105vh',
-        left: 'calc(50% - 7vw - 10px)',
+        x: 'calc(50% - 7vw - 10px)',
         transform: 'rotate(0)',
         ease: 'power1.out',
         duration: animationDurations.FAST,
