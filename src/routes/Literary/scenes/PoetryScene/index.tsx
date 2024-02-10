fcd5804f22ddb5b76ca897dd183d2970486c2f34
Alvin Teh <i@alvinteh.me>
2024-02-10 23:38:55 +0800

    Updated Works display and animation to be a slider

--- src/routes/Literary/scenes/PoetryScene/index.tsx
+++ src/routes/Literary/scenes/PoetryScene/index.tsx
@@ -1,7 +1,8 @@
 import { gsap } from 'gsap';
+import { Observer } from 'gsap/Observer';
 import { SplitText } from 'gsap/SplitText';
 import { useGSAP } from '@gsap/react';
-import { useContext, useMemo, useRef, useState } from 'react';
+import { useContext, useEffect, useMemo, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { v4 as uuid } from 'uuid';
 
@@ -14,6 +15,10 @@ import rawWorks from './data/work-data.json';
 import { Work } from './types';
 
 import SceneBackground from './images/scene-background.jpg';
+
+interface WorksAttrs {
+  $scrollPosition: number;
+}
 
 interface WorkAttrs {
   $backgroundImage: string;
@@ -47,33 +52,51 @@ const Header = styled.h2`
   }
 `;
 
+const WriteupWrapper = styled.div<{ $isVisible: boolean }>`
+  display: grid;
+  margin: 0 0 4rem;
+  max-width: 50rem;
+  grid-template-rows: ${(props) => { return props.$isVisible ? 1 : 0; }}fr;
+  opacity: ${(props) => { return props.$isVisible ? 1 : 0; }};
+  transition: opacity ${animationDurations.FAST}s ${cubicBezier},
+    margin-bottom ${animationDurations.MEDIUM}s ${cubicBezier} ${animationDurations.FAST}s;
+    grid-template-rows ${animationDurations.MEDIUM}s ${cubicBezier} ${animationDurations.FAST}s;
+`;
+
 const Writeup = styled.p`
-  margin: 0 0 5rem;
   font-family: 'Crimson Text', serif;
   font-size: 1.6rem;
   font-weight: 600;
   line-height: 1.8rem;
+  overflow: hidden;
   text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.7);
+`;
+
+const WorksSlider = styled.div`
+  position: relative;
   max-width: 50rem;
 `;
 
 const WorksWrapper = styled.div`
-  max-width: 50rem;
-`;
-
-const Works = styled.ul`
-  display: grid;
-  margin: 0;
-  box-sizing: border-box;
   border-top: solid 1px rgba(255, 255, 255, 0.5);
   border-bottom: solid 1px rgba(255, 255, 255, 0.5);
-  padding: 20px 0;
-  max-height: 297px;
+  padding: 25px 0;
+  height: 255px;
+  overflow: hidden;
+`;
+
+const Works = styled.ul.attrs<WorksAttrs>(({ $scrollPosition }) => ({
+  style: {
+    transform: `translate3d(${$scrollPosition}px, 0, 0)`,
+  }
+}))<{ $isVisible: boolean }>`
+  display: grid;
+  margin: 0;
+  padding: 0;
+  column-gap: 45px;
+  grid-auto-flow: column;
   list-style: none;
-  grid-template-columns: repeat(auto-fill, 165px);
-  justify-content: space-between;
-  overflow-y: hidden;
-  scrollbar-gutter: stable;
+  transition: transform ${animationDurations.MEDIUM}s ${cubicBezier};
 `;
 
 const Work = styled.li.attrs<WorkAttrs>(({ $backgroundImage }) => ({
@@ -81,7 +104,7 @@ const Work = styled.li.attrs<WorkAttrs>(({ $backgroundImage }) => ({
     backgroundImage: `url(${$backgroundImage})`,
   }
 }))`
-  margin: 0 0 20px;
+  margin: 0;
   padding: 0;
   width: 165px;
   height: 255px;
@@ -94,23 +117,48 @@ const Work = styled.li.attrs<WorkAttrs>(({ $backgroundImage }) => ({
   overflow: hidden;
   text-indent: 100%;
   white-space: nowrap;
-  transition: box-shadow ${animationDurations.XFAST}s ${cubicBezier};
+  transition: all ${animationDurations.FAST}s ${cubicBezier};
 
   &:hover {
     filter: brightness(1) !important;
-    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.7); 
+    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.7);
+    transform: scale3d(1.05, 1.05, 1.05);
   }
 `;
 
-const ViewMoreButton = styled.a`
+const ButtonBase = styled.a<{ $isEnabled: boolean }>`
   display: block;
-  margin-top: 5px;
-  color: #ffffff;
-  content: "Scroll for More";
-  font-weight: 500;
-  text-align: center;
-  text-transform: uppercase;
-  user-select: none;
+  position: absolute;
+  top: 50%;
+  border-top: 16px solid transparent;
+  border-bottom: 16px solid transparent;
+  width: 0; 
+  height: 0;
+  cursor: pointer;
+  opacity: ${(props) => { return props.$isEnabled ? 1 : '0 !important'; }};
+  overflow: hidden;
+  text-indent: 100%;
+  white-space: nowrap;
+  transform: translate3d(0, -50%, 0);
+  transition: all ${animationDurations.FAST}s;
+`;
+
+const PrevButton = styled(ButtonBase)`
+  left: -20px;
+  border-right: 10px solid rgba(255, 255, 255, 0.75);
+
+  &:hover {
+   border-right-color: rgba(255, 255, 255, 1); 
+  }
+`;
+
+const NextButton = styled(ButtonBase)`
+  right: -20px;
+  border-left: 10px solid rgba(255, 255, 255, 0.75); 
+
+  &:hover {
+    border-left-color: rgba(255, 255, 255, 1); 
+  }
 `;
 
 const extWorks: Work[] = (rawWorks as unknown) as Work[];
@@ -120,22 +168,93 @@ extWorks.forEach((work: Work) => {
 });
 
 const PoetryScene = ({ sceneIndex }: { sceneIndex: number }) => {
-  const { registerScene } = useContext(PageContext);
+  const { pageObserverName, registerScene } = useContext(PageContext);
   const [works] = useState<Work[]>(extWorks);
+  const [worksScrollPosition, setWorksScrollPosition] = useState<number>(0);
+  const [isPrevButtonEnabled, setIsPrevButtonEnabled] = useState<boolean>(false);
+  const [isNextButtonEnabled, setIsNextButtonEnabled] = useState<boolean>(true);
+  const [isDetailViewActive, setIsDetailViewActive] = useState<boolean>(false);
 
   // Screen refs
   const screenRef = useRef<HTMLDivElement>() as React.MutableRefObject<HTMLDivElement>;
   const headerRef = useRef<HTMLHeadingElement>() as React.MutableRefObject<HTMLHeadingElement>;
+  const writeupWrapperRef = useRef<HTMLDivElement>() as React.MutableRefObject<HTMLDivElement>;
   const writeupRef = useRef<HTMLParagraphElement>() as React.MutableRefObject<HTMLParagraphElement>;
+  const worksWrapperRef = useRef<HTMLDivElement>() as React.MutableRefObject<HTMLDivElement>;
   const worksRef = useRef<HTMLUListElement>() as React.MutableRefObject<HTMLUListElement>;
   const workRefs = useRef<HTMLLIElement[]>([]);
-  const viewMoreButtonRef = useRef<HTMLAnchorElement>() as React.MutableRefObject<HTMLAnchorElement>;
+  const prevButtonRef = useRef<HTMLAnchorElement>() as React.MutableRefObject<HTMLAnchorElement>;
+  const nextButtonRef = useRef<HTMLAnchorElement>() as React.MutableRefObject<HTMLAnchorElement>;
 
   gsap.registerPlugin(SplitText);
 
-  const handleWorkClick = (work: Work): void => {
-    // WIP
+  const handlePrevButtonClick = (): void => {
+    const worksElement: HTMLUListElement = worksRef.current;
+    const scrollLimit = 0;
+
+    if (worksScrollPosition < scrollLimit) {
+      const newScrollPosition: number = Math.min(worksScrollPosition + worksElement.clientWidth, scrollLimit);
+
+      // Remove inline opacity set by GSAP
+      nextButtonRef.current.style.removeProperty('opacity');
+
+      setWorksScrollPosition(newScrollPosition);
+      setIsNextButtonEnabled(true);
+
+      if (newScrollPosition === scrollLimit) {
+        setIsPrevButtonEnabled(false);
+      }
+    }
   };
+
+  const handleNextButtonClick = (): void => {
+    const worksElement: HTMLUListElement = worksRef.current;
+    const scrollLimit: number = worksElement.clientWidth - worksElement.scrollWidth;
+
+    if (worksScrollPosition > scrollLimit) {
+      const newScrollPosition: number = Math.max(worksScrollPosition - worksElement.clientWidth, scrollLimit);
+
+      // Remove inline opacity set by GSAP
+      prevButtonRef.current.style.removeProperty('opacity');
+
+      setWorksScrollPosition(newScrollPosition);
+      setIsPrevButtonEnabled(true);
+
+      if (newScrollPosition === scrollLimit) {
+        setIsNextButtonEnabled(false);
+      }
+    }
+  };
+
+  const handleWorkClick = (work: Work): void => {
+    // DONE: Hide <Writeup>
+    // DONE: Shift <WorksWrapper> up
+
+    // Hide <Works>
+    // Expand <WorkView>
+
+    setIsDetailViewActive(true);
+
+    // TO-DO: Load and render markdown
+    console.log(work);
+
+    // TO-DO: Reset changes when leaving detail view or scene (setIsDetailViewActive = false)
+  };
+
+  useEffect((): void => {
+    const observer: Observer | undefined = Observer.getById(pageObserverName);
+
+    if (!observer) {
+      return;
+    }
+
+    if (isDetailViewActive) {
+      observer.disable();
+    }
+    else {
+      observer.enable();
+    }
+  }, [pageObserverName, isDetailViewActive]);
 
   // Screen animation
   useGSAP((): void => {
@@ -168,30 +287,28 @@ const PoetryScene = ({ sceneIndex }: { sceneIndex: number }) => {
       stagger: animationDurations.XXFAST,
     });
 
-    const worksElement: HTMLUListElement = worksRef.current;
-
-    timeline.from(worksElement, {
+    timeline.from(worksWrapperRef.current, {
       opacity: 0,
       ease: 'power1.inOut',
       duration: animationDurations.FAST,
     });
 
-    timeline.from(worksElement, {
+    timeline.from(worksWrapperRef.current, {
       padding: 0,
-      maxHeight: 0,
+      height: 0,
       ease: 'power1.inOut',
       duration: animationDurations.MEDIUM,
     });
 
     const workElements: HTMLLIElement[] = workRefs.current;
-    const firstLineY: number = workElements[0].getBoundingClientRect().y;
+    const xLimit: number = worksRef.current.getBoundingClientRect().right;
 
-    const firstLineWorkElements: HTMLLIElement[] = [].slice.call(workElements)
+    const initialWorkElements: HTMLLIElement[] = [].slice.call(workElements)
       .filter((workElement: HTMLLIElement): boolean => {
-        return workElement.getBoundingClientRect().y === firstLineY;
+        return workElement.getBoundingClientRect().left < xLimit;
       });
 
-    timeline.fromTo(firstLineWorkElements, {
+    timeline.fromTo(initialWorkElements, {
       filter: 'brightness(0)',
       opacity: 0,
     },
@@ -203,68 +320,13 @@ const PoetryScene = ({ sceneIndex }: { sceneIndex: number }) => {
       stagger: animationDurations.XFAST,
     });
 
-    timeline.from(viewMoreButtonRef.current, {
+    timeline.from([prevButtonRef.current, nextButtonRef.current], {
       opacity: 0,
       ease: 'power1.inOut',
       duration: animationDurations.FAST,
     });
 
     timeline.addLabel(`scene-${sceneIndex}-intro`);
-
-    timeline.to(viewMoreButtonRef.current, {
-      autoAlpha: 0,
-      ease: 'power1.inOut',
-      duration: animationDurations.FAST,
-    });
-
-    timeline.to(writeupRef.current.children, {
-      filter: 'blur(0.5rem)',
-      opacity: 0,
-      ease: 'power1.inOut',
-      duration: animationDurations.MEDIUM,
-    });
-
-    const yDifference: number = writeupRef.current.getBoundingClientRect().top -
-      worksElement.getBoundingClientRect().top;
-    // Note, we deduct 50px to accommodate for the bottom padding
-    // We can ignore the linting issue as the parent element (PaddedPageWrapper) always exists
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const newHeight: number = worksElement.parentElement!.parentElement!.getBoundingClientRect().bottom
-      - writeupRef.current.getBoundingClientRect().top - 50;
-
-    timeline.to(worksElement.parentElement, {
-      y: yDifference,
-      ease: 'power1.inOut',
-      duration: animationDurations.FAST,
-    });
-
-    timeline.to(worksElement, {
-      maxHeight: `${newHeight}px`,
-      ease: 'power1.inOut',
-      duration: animationDurations.SLOW,
-    }, '<');
-
-    const remainingWorkElements: HTMLLIElement[] = [].slice.call(workElements)
-      .filter((workElement: HTMLLIElement): boolean => {
-        const y: number = workElement.getBoundingClientRect().y;
-        return y > firstLineY && y < worksElement.clientHeight;
-      });
-
-    timeline.fromTo(remainingWorkElements, {
-      filter: 'brightness(0)',
-      opacity: 0,
-    },
-    {
-      filter: 'brightness(0.9)',
-      opacity: 1,
-      ease: 'power1.inOut',
-      duration: animationDurations.MEDIUM,
-      stagger: animationDurations.XFAST,
-    }, `<+=${animationDurations.XFAST}`);
-
-    timeline.set(worksElement, { overflowY: 'auto' });
-
-    timeline.addLabel(`scene-${sceneIndex}-outro`);
 
     registerScene(sceneIndex, screenRef, timeline, 'Expressions of Life');
   }, [registerScene, sceneIndex, works]);
@@ -293,19 +355,24 @@ const PoetryScene = ({ sceneIndex }: { sceneIndex: number }) => {
     <Screen innerRef={screenRef} backgroundImage={SceneBackground}>
       <PaddedPageWrapper>
         <Header ref={headerRef}>Painting Life with Words</Header>
-        <Writeup ref={writeupRef}>
-        As someone predisposed towards rumination, I often indulge in intellectual musings and pen my thoughts for later
-        reflection. Literature is my preferred medium of expression, and poetry is my song.
-        <br />
-        <br />
-        Below is a curated list of my writing.
-        </Writeup>
-        <WorksWrapper>
-          <Works ref={worksRef}>
-            {workElements}
-          </Works>
-          <ViewMoreButton ref={viewMoreButtonRef}>Scroll for More</ViewMoreButton>
-        </WorksWrapper>
+        <WriteupWrapper ref={writeupWrapperRef} $isVisible={!isDetailViewActive}>
+          <Writeup ref={writeupRef}>
+          As someone predisposed towards rumination, I often indulge in intellectual musings and pen my thoughts for later
+          reflection. Literature is my preferred medium of expression, and poetry is my song.
+          <br />
+          <br />
+          Below is a curated list of my writing.
+          </Writeup>
+        </WriteupWrapper>
+        <WorksSlider>
+          <WorksWrapper ref={worksWrapperRef}>
+            <Works ref={worksRef} $scrollPosition={worksScrollPosition} $isVisible={!isDetailViewActive}>
+              {workElements}
+            </Works>
+            <PrevButton ref={prevButtonRef} $isEnabled={isPrevButtonEnabled} onClick={handlePrevButtonClick}>Prev</PrevButton>
+            <NextButton ref={nextButtonRef} $isEnabled={isNextButtonEnabled} onClick={handleNextButtonClick}>Next</NextButton>
+          </WorksWrapper>
+        </WorksSlider>
       </PaddedPageWrapper>
     </Screen>
   );
